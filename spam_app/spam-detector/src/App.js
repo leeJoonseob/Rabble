9159cdb32724b9f37b57b019689dd4f22cb107f2
1di0t <imsunload@gmail.com>
2024-11-11 16:07:33 +0900

    refactor: change port to 8080

--- spam_app/spam-detector/src/App.js
+++ spam_app/spam-detector/src/App.js
@@ -14,7 +14,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('http://127.0.0.1:8080/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
