3c28d1622fddd32367465d9f529bfae0e4df29bf
1di0t <imsunload@gmail.com>
2024-11-29 17:18:53 +0900

    Refactor: modify spam dataset 메일내용->메일제목

--- pre_processing/data_preprocessing.py
+++ pre_processing/data_preprocessing.py
@@ -14,57 +14,32 @@ class KiwiSingleton:
         return cls._instance
 
 def combine_data():
-    """
-    
-    """
-    # 데이터 불러오기
+    # 학습 데이터 로드
     google_message = pd.read_csv("Rabble/datasets/messages.csv")
     spam_message = pd.read_csv("Rabble/datasets/spam_virus.csv")
-
-    selected_columns = google_message.iloc[:, [0, 1, 2]] 
-
-
-    # '수신일자 '수신시간'을 합쳐 '날짜' 열 생성
-    spam_message['날짜'] = pd.to_datetime(spam_message['수신일자'] + ' ' + spam_message['수신시간'], format='%Y-%m-%d %H:%M')
-    spam_message = spam_message.drop(['수신일자', '수신시간'], axis=1)
-
-    # 수신시간을 저장할 새로운 열 생성
-    google_message['수신시간'] = None
-
-    # 수신시간 추출 함수 정의 (메일내용에서 '날짜:' 패턴을 찾아 시간 추출)
-    def extract_time(mail_content):
-        # 예시 패턴: '날짜: 2024. 10. 15. 오전 12:00'
-        match = re.search(r'날짜:\s*\d{4}\.\s*\d{1,2}\.\s*\d{1,2}\.\s*(오전|오후)\s*(\d{1,2}):(\d{2})', mail_content)
-        if match:
-            period, hour, minute = match.groups()
-            hour = int(hour)
-            if period == '오후' and hour != 12:
-                hour += 12
-            elif period == '오전' and hour == 12:
-                hour = 0
-            return f"{hour:02d}:{minute}"
-        return None
-
-    # '내용' 열을 사용하여 '수신시간' 추출
-    google_message['수신시간'] = google_message['메일내용'].apply(extract_time)
-
-    if google_message['날짜'].dtype == 'object':
-        google_message['날짜'] = pd.to_datetime(google_message['날짜'])
-
-    if spam_message['날짜'].dtype == 'object':
-        spam_message['날짜'] = pd.to_datetime(spam_message['날짜'])
-
+    spam_msg = pd.read_csv("Rabble/datasets/spam_dataset.csv")
+    
+    # 메일 종류 추가
     google_message['메일종류'] = '햄'
+    spam_message['메일종류'] = '스팸'
+    spam_msg['메일종류'] = '스팸'
 
     # 필요한 열 선택
-    final_google_df = google_message[['날짜', '메일종류', '메일제목']]
-    final_spam_df = spam_message[['날짜','메일종류', '메일제목']]
+    final_google_df = google_message[['메일종류', '메일제목']]
+    final_spam_df = spam_message[['메일종류', '메일제목']]
+    final_spam_msg_df = spam_msg[['메일종류', '메일제목']]
+
+
+    # Ham 데이터 수
+    ham_count = final_google_df.shape[0]
+
+    # Spam 데이터 언더샘플링
+    final_spam_df = final_spam_df.sample(n=ham_count, random_state=42)
 
     # 데이터 결합
-    combined_df = pd.concat([final_google_df, final_spam_df], axis=0, ignore_index=True)
+    combined_df = pd.concat([final_google_df, final_spam_df, final_spam_msg_df], axis=0, ignore_index=True)
 
-    print(combined_df.head())
-    combined_df.to_csv('/Users/PROTEIN/Desktop/Coll_Third/spam_finder/Rabble/datasets/combined_data.csv', index=False, encoding='utf-8')
+    combined_df.to_csv('/Users/PROTEIN/Desktop/Coll_Third/spam_finder/Rabble/datasets/combined_data_under.csv', index=False, encoding='utf-8')
 
 def tokenize_kiwi(text):
     kiwi = KiwiSingleton.get_instance()
@@ -88,3 +63,5 @@ class TextEncoder:
             raise ValueError("Tokenizer not fitted. Call fit() first.")
         sequences = self.tokenizer.texts_to_sequences(text)
         return sequence.pad_sequences(sequences, maxlen=self.max_len, padding='post', truncating='post')
+
+combine_data()
